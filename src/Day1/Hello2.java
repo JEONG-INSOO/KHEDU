ac74f28a6183cb66747179f8add030a2038e5932
JEONG-INSOO <mocca3232@naver.com>
2023-06-08 14:49:02 +0900

    first day commit

---
+++ src/Day1/Hello2.java
@@ -0,0 +1,24 @@
+package Day1;
+/*
+ 여러줄 주석
+ */
+// 한줄 주석
+
+//클래스 정의
+public class Hello2 {
+  public static void sum(int n, int m) {
+    System.out.println("sum 호출됨");
+  }
+
+  public static int sum2(int n, int m) {
+    System.out.println("sum2() 호출됨");
+    return n + m ;
+  }
+  // 실행 메소드 정의
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+    sum(10,20);
+    sum2(30,40);
+
+  }
+}
--- src/sec01_datatype/EX01_UsageOfDataType/UsageOfDataType.java
+++ src/sec01_datatype/EX01_UsageOfDataType/UsageOfDataType.java
@@ -1,6 +1,6 @@
 package sec01_datatype.EX01_UsageOfDataType;
 
-public class UsagaOfDataType {
+public class UsageOfDataType {
   public static void main(String[] args) {
     // 변수 선언과 함께 값 대입
     int a = 3;
@@ -9,5 +9,14 @@ public class UsagaOfDataType {
     b = 4;
     System.out.println(a);
     System.out.println(b);
+
+    System.out.println(2+3);
+    System.out.println(a+b); // 덧셈
+    System.out.println(a-b); // 뺄셈
+    System.out.println(a*b); // 곱셈
+    System.out.println(a/b); // 나눗셈
+    System.out.println(a%b); // 나머지
+
   }
 }
+
