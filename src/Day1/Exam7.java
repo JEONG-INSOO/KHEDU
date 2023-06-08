9d02205bfd31ffd650e490cb1c68d6945cd2bb5d
JEONG-INSOO <mocca3232@naver.com>
2023-06-08 17:30:47 +0900

    first day commit

---
+++ src/Day1/Exam7.java
@@ -0,0 +1,31 @@
+package Day1;
+
+import java.sql.SQLOutput;
+
+public class Exam7 {
+  public static void main(String[] args) {
+    int kor = 95;
+//    boolean flag = kor >=90;
+    //단순 if
+//    if(kor >=90){
+//      System.out.println("90 이상이다");
+    //if else
+    if(kor >= 90){
+      System.out.println("90 이상이다");
+    }else {
+      System.out.println("90 미만이다");
+    }
+    //if else if else
+    if(kor >= 90) {
+      System.out.println("수");
+    } else if (kor >= 80) {
+      System.out.println("우");
+    } else if (kor >= 70) {
+      System.out.println("미");
+    } else if (kor >= 60) {
+      System.out.println("양");
+  } else {
+      System.out.println("가");
+   }
+  }
+}
