5972f3cb7887e729425efbf10e06156c98fb017d
JEONG-INSOO <mocca3232@naver.com>
2023-06-29 19:56:16 +0900

    DAY16 commit

--- src/com/kh/dic/Dictionary.java
+++ src/com/kh/dic/Dictionary.java
@@ -1,20 +1,33 @@
 package com.kh.dic;
 
+import java.util.Scanner;
 import java.util.TreeMap;
 
 public class Dictionary {
-  private TreeMap<String, String> dic;
+  Scanner scanner = new Scanner(System.in);
+  public TreeMap<String, String> dic;
 
-  public Dictionary(TreeMap<String, String> dic) {
+
+  public Dictionary() {
     this.dic = dic;
   }
 
   //저장
-  public void save(String word, String meaning) {
+  public void save() {
+    System.out.println("입력할 단어를 입력하세요.");
+    String word = scanner.nextLine();
+    System.out.println("입력한 단어의 의미를 입력하세요.");
+    String meaning = scanner.nextLine();
+
     // 중복체크
     if (dic.containsKey(word)) {
-      throw new RuntimeException("중복입니다 !");
+      throw new RuntimeException("중복된 단어입니다.");
     }
-    dic.put(word, meaning);
+    //최대 5개
+    if (dic.size() >= 5) {
+      throw new RuntimeException("단어는 최대 5개까지 가능합니다.");
+    }
+    this.dic.put(word, meaning);
+    System.out.println("단어 저장 완료!");
   }
 }
--- src/com/kh/dic/DictionaryMain.java
+++ src/com/kh/dic/DictionaryMain.java
@@ -1,22 +1,53 @@
 package com.kh.dic;
 
+import com.kh.dic.Dictionary;
+
+import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 public class DictionaryMain {
   public static void main(String[] args) {
-    Dictionary dic = new Dictionary();
+    com.kh.dic.Dictionary dic = new Dictionary();
     Scanner scanner = new Scanner(System.in);
-    System.out.println("메뉴 ->");
     boolean stop = false;
-    do {
-      switch () {
-        case 1:
-          break;
-        case 2:
-          break;
-      }while(!stop);
+    while (!stop) {
+      try {
+        System.out.println("1.저장 2.검색 3.수정 4.삭제 5.목록 6.색인 7.통계 8.종료");
+        System.out.print("선택 >> ");
+        String menu = scanner.nextLine();
+
+        String word = null;     //단어
+        String meaning = null;  //의미
+        switch (menu) {
+          case "1": //저장
+            try {
+            dic.save();
+            } catch (RuntimeException e) {
+              System.out.println(e.getMessage());
+            }
+            break;
+          case "2": //검색
+            break;
+          case "3": //수정
+            break;
+          case "4": //삭제
+            break;
+          case "5": //목록
+            break;
+          case "6": //색인
+            break;
+          case "7": //통계
+            break;
+          case "8": // 종료
+            stop = true;
+            continue;
+          default:
+        }
+      } catch (Exception e) {
+        System.out.println(e.getMessage());
+      }
     }
+    System.out.println("프로그램 종료!");
   }
 }
-
-
