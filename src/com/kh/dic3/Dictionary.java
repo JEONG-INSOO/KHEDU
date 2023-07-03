eebdec6e7e06c980c878a7fed016fee0b8fcf8ab
JEONG-INSOO <mocca3232@naver.com>
2023-07-03 17:30:53 +0900

    DAY 17 COMMIT

---
+++ src/com/kh/dic3/Dictionary.java
@@ -0,0 +1,168 @@
+package com.kh.dic3;
+
+import com.sun.source.tree.Tree;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Dictionary extends TreeMap<String, String> {
+    private static final int WORD_CAPACITY = 5; // 단어 최대 저장 갯수
+
+    public static final int ASC_SORT  = 1;    // 1:오름차순 2:내림차순
+    public static final int DESC_SORT =  2;
+
+    public Dictionary() {
+        super();
+        init();
+    }
+
+    //저장
+    public void save(String word, String meaning) {
+        String wordToLowerCase = word.toLowerCase();
+        //1) 단어수 초과
+        if (size() == WORD_CAPACITY) {
+            throw new DictionaryException("최대 단어수 초과!");
+        }
+        ;
+        //2) 단어 중복체크
+        if (containsKey(wordToLowerCase)) {
+            throw new DictionaryException("이미 등록되었습니다!");
+        }
+        put(wordToLowerCase, meaning);
+    }
+
+    //검색
+    public Map<String, String> findByWord(String word) {
+        String wordToLowerCase = word.toLowerCase();
+        Map<String, String> map = new HashMap<>();
+
+        //1)일치하는 단어를 검색
+        if (containsKey(wordToLowerCase)) {
+            String meaning = get(wordToLowerCase);
+            map.put(wordToLowerCase, meaning);
+            return map;
+        }
+        //2)일치하는 단어가 없을경우. 즉, 범위검색  ex) sm
+        String frontWord = wordToLowerCase.substring(0, wordToLowerCase.length() - 1);  // "s"
+        char lastLetter = wordToLowerCase.charAt(wordToLowerCase.length() - 1); // 단어의 마지막문자. 'm'
+        char nextLetter = (char) (lastLetter + 1); // 단어의 마지막문자의 다음문자 'n'
+        String nextForntWord = frontWord + String.valueOf(nextLetter); // "sn"
+        map = subMap(wordToLowerCase, true, String.valueOf(nextForntWord), false);
+
+        //3) 범위검색에도 포함되지 않으면
+        if (map.size() == 0) {
+            throw new DictionaryException("검색하고자 하는 단어가 없습니다.");
+        }
+        return map;
+    }
+
+    //수정
+    public boolean update(String word, String meaning) {
+        boolean flag = false;
+        String wordToLowerCase = word.toLowerCase();
+
+        if (replace(wordToLowerCase, meaning) == null) {
+            throw new DictionaryException("수정하고자 하는 단어가 없습니다.");
+        } else {
+            flag = true;
+        }
+        return flag;
+    }
+
+    //삭제
+    public boolean delete(String word) {
+        boolean flag = false;
+        String wordToLowerCase = word.toLowerCase();
+
+        if (remove(wordToLowerCase) == null) {
+            throw new DictionaryException("삭제하고자 하는 단어가 없습니다.");
+        } else {
+            flag = true;
+        }
+        return flag;
+    }
+
+    //목록
+    public Map<String, String> listAll(int sort) {
+        Map<String, String> map = null;
+        switch (sort) {
+            case ASC_SORT :   // 오름차순
+                map = this;
+                break;
+            case DESC_SORT:_SORT : //내림차순
+            map = this.descendingMap();
+                break;
+            default:
+                throw new DictionaryException("정렬방법이 잘못 지정되었습니다.(1: 오름차순, 2:내림차순)");
+        }
+        return map;
+    }
+
+    //색인
+    public Map<String,String> index(char ch) {
+        char nextLetter = (char)(ch + 1);
+        Map<String,String> map = subMap(String.valueOf(ch),true,String.valueOf(nextLetter),false);
+        if (map.size() == 0) {
+            throw new DictionaryException("초성값으로 시작하는 단어가 없습니다.");
+        }
+        return map;
+    }
+
+    //통계
+    public String showWordStatics() {
+        String info = "";
+        info += "1.저장된 단어의 갯수 : " + size() + "\n";
+        info += "2.단어의 문자수가 가장 많은 단어 : " + getMostCharactorCount() + "\n";
+        info += "3.단어 글자수 내림차순 출력(단어만) : \n" + decendingByLengthOfWord() + "\n";
+
+        return info;
+    }
+
+    //2.단어의 문자수가 가장 많은 단어
+    private String getMostCharactorCount() {
+        Set<String> words = keySet();
+
+        int maxLength = 0;
+        String theLongestWord = "";
+        for (String word : words){
+            if(maxLength < word.length()){
+                //maxLength = word.length();
+                theLongestWord = word;
+            }
+        }
+        return theLongestWord;
+    }
+    //3.단어 글자수 내림차순 출력(단어만)
+    private String decendingByLengthOfWord() {
+        String str = "";
+        // TreeMap의 정렬기준을 생성자를 통해 개발자 의도대로 변경할 수 있다.
+        TreeMap<String,String> map = new TreeMap<>(new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                int result = 1;
+                if(o1.length() > o2.length()){
+                    result = -1; // 음수
+                }else if(o1.length() < o2.length()){
+                    result = 1; // 양수
+                }
+                return result;
+            }
+        });
+        map.putAll(this);   // 새로 정의된 맵으로 재정렬
+        for(String word : map.keySet()){
+            str += word + "\n";
+        }
+        return str;
+    }
+
+    //초기값
+    public void init() {
+        put("student", "학생");
+        put("teacher", "교사");
+        put("classroom", "교실");
+        put("smart", "현명한");
+        put("lunch", "점심");
+    }
+
+}
---
+++ src/com/kh/dic3/DictionaryMain.java
@@ -0,0 +1,117 @@
+package com.kh.dic3;
+
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Set;
+
+public class DictionaryMain {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        Dictionary dic = new Dictionary();
+        boolean stop = false;
+
+        do {
+            try {
+                System.out.println("1.저장 2.검색 3.수정 4.삭제 5.목록 6.색인 7.통계 8.종료 9.초기화 10.비우기");
+                System.out.print("선택 >> ");
+                String menu = scanner.nextLine();
+                String word = "";       //단어
+                String meaning = "";    //의미
+                Map<String, String> findedWords = null; // 검색된 단어목록;
+                switch (menu) {
+                    case "1": //저장
+                        System.out.print("단어 입력 : ");
+                        word = scanner.nextLine();
+                        System.out.print("의미 입력 : ");
+                        meaning = scanner.nextLine();
+                        //          try {
+                        dic.save(word, meaning);
+                        //          } catch (DictionaryException e) {
+                        //            System.out.println(e.getMessage());
+                        //          }
+                        break;
+                    case "2": //검색
+                        System.out.print("단어 입력 : ");
+                        word = scanner.nextLine();
+                        findedWords = dic.findByWord(word);
+                        //case1) keyset
+                        Set<String> words = findedWords.keySet();
+                        for (String key : words) {
+                            System.out.printf("%s : %s\n", key, findedWords.get(key));
+                        }
+                        //case2) entry
+                        //          Set<Map.Entry<String, String>> entries = findedWords.entrySet();
+                        //          for (Map.Entry<String, String> entry : entries) {
+                        //            System.out.printf("%s : %s\n",entry.getKey(),entry.getValue());
+                        //          }
+
+                        break;
+                    case "3":  //수정
+                        System.out.print("단어 입력 : ");
+                        word = scanner.nextLine();
+                        System.out.print("의미 입력 : ");
+                        meaning = scanner.nextLine();
+                        if (dic.update(word, meaning)) {
+                            System.out.println("단어를 수정하였습니다.!");
+                        }
+                        break;
+                    case "4": //삭제
+                        System.out.print("단어 입력 : ");
+                        word = scanner.nextLine();
+                        if (dic.delete(word)) {
+                            System.out.println("단어를 삭제하였습니다.!");
+                        }
+                        break;
+                    case "5": //목록
+                        System.out.println("1.오름차순 2.내림차순");
+                        System.out.print("선택 >> ");
+                        String submenu = scanner.nextLine();
+
+                        switch (submenu) {
+                            case "1":
+                                findedWords = dic.listAll(Dictionary.ASC_SORT);
+                                break;
+                            case "2":
+                                findedWords = dic.listAll(Dictionary.DESC_SORT);
+                                break;
+                        }
+                        for (String key : findedWords.keySet()) {
+                            System.out.printf("%s : %s\n", key, findedWords.get(key));
+                        }
+
+                        break;
+                    case "6": // 색인
+                        do {
+                            System.out.print("초성 입력 : ");
+                            word = scanner.nextLine();
+                            findedWords = dic.index(word.charAt(0));
+                            for (String key : findedWords.keySet()) {
+                                System.out.printf("%s : %s\n", key, findedWords.get(key));
+                            }
+                        }while(word.length() != 1);
+
+                        break;
+                    case "7": //통계
+                        String statics = dic.showWordStatics();
+                        System.out.println(statics);
+                        break;
+                    case "8": //종료
+                        stop = true;
+                        break;
+                    case "9": //초기화
+                        dic.init();
+                        break;
+                    case "10": //비우기
+                        dic.clear();
+                        break;
+                    default:
+                        System.out.println("잘못된 메뉴 선택!(1~8)");
+                }
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        } while (!stop);
+        System.out.println("영어 단어장을 종료합니다.");
+    }
+}
